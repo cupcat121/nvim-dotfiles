d9aa23acc2c842fbaa1190421aada7c198f6b7aa
uncle_hoshino <hgenaaaa@gmail.com>
2023-12-09 21:17:14 +0800

    add treesitter support for hyprland conf

--- plugins/init.lua
+++ plugins/init.lua
@@ -69,7 +69,18 @@ require("lazy").setup(
             -- code parsing system and better highlighting
             'nvim-treesitter/nvim-treesitter',
             enabled = true,
-            build = ":TSUpdate"
+            build = ":TSUpdate",
+            init = function()
+                local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+                parser_config.hypr = {
+                    install_info = {
+                        url = "https://github.com/luckasRanarison/tree-sitter-hypr",
+                        files = { "src/parser.c" },
+                        branch = "master",
+                    },
+                    filetype = "hypr",
+                }
+            end
         },
 
         -- colorscheme
