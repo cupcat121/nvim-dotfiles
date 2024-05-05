dde36690d79d056e38d63f06443e372e93ea7009
cupcat121 <hgenaaaa@gmail.com>
2024-05-05 13:41:08 +0800

    update noise.vim cofig

--- plugins/noise-nvim/init.lua
+++ plugins/noise-nvim/init.lua
@@ -7,6 +7,20 @@ return {
 	opts = {},
 	dependencies = {
 		'MunifTanjim/nui.nvim',
-		'rcarriga/nvim-notify',
+		{
+            'rcarriga/nvim-notify',
+            config = function ()
+                local notify = require('notify')
+                notify.setup({
+                    timeout = 2500,
+                    max_width = 400,
+                    max_height = 1000,
+                    minimum_width = 40,
+                    stages = "fade",
+                    render = "compact",
+                    fps = 40,
+                })
+            end,
+        },
 	},
 }
